e6240a47d324e684d8dd9c6997142878e571ca00
DeliriumTraemens <oskinnick@gmail.com>
2022-01-04 20:59:46 +0300

    OpenGraph MetaDto Back Finished

--- src/main/java/org/mykola/sarafan3/dto/MetaDto.java
+++ src/main/java/org/mykola/sarafan3/dto/MetaDto.java
@@ -1,8 +1,10 @@
 package org.mykola.sarafan3.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class MetaDto {
 	private String title;
 	private String description;
