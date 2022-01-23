b10d13bb3716f8d9190556af70c39a643a2f0f64
DeliriumTraemens <oskinnick@gmail.com>
2022-01-23 19:22:46 +0300

    Item Working, Picture Working, CategoryForm.vue Dev Start Category without Entity

--- src/main/java/org/mykola/sarafan3/domain/Category.java
+++ src/main/java/org/mykola/sarafan3/domain/Category.java
@@ -1,11 +1,23 @@
 package org.mykola.sarafan3.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name ="category")
 public class Category {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private Long parentCatId;
 	private String name;
@@ -16,8 +28,11 @@ public class Category {
 	
 	private boolean root;
 	
-	private Category parent;
+	@JsonIgnore
+	@ManyToOne
+	private Category parentCat;
 	
+	@OneToMany(mappedBy="parentCat")
 	private Set<Category> childrenSet = new HashSet<Category>();
 	
 	
